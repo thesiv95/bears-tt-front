ab9d7347a0809508787feaf84446aa991d3caace
Ilya Soloveychik <dj.active.17@gmail.com>
2023-07-19 15:55:56 +0700

    feat: add vk bridge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import bridge from '@vkontakte/vk-bridge';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,3 +14,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+bridge.send("VKWebAppInit");
+if (bridge.supports("VKWebAppResizeWindow")) {
+    bridge.send("VKWebAppResizeWindow", { "width": 800, "height": 1000 });
+}
