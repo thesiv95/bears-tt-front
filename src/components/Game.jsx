aa9b5359e9514e51af48d0c620e065ad0b3cb3e6
Ilya Soloveychik <dj.active.17@gmail.com>
2023-07-19 15:17:21 +0700

    todo: fix wheel start

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,49 +1,65 @@
 import React from 'react'
+import { TweenMax } from 'gsap'
 import gameCSS from '../css/game.module.css'
 import doAPIRequest from '../utils/doAPIRequest';
-import Wheel from './Game/Wheel';
+import Winwheel from 'winwheel';
+import wheelParams from '../utils/wheelParams';
 
 const Game = () => {
-
-
+	const [wheelSpinning, setWheelSpinning] = React.useState(false);
 	const [jackpot, setJackpot] = React.useState(0);
 	const [balance, setBalance] = React.useState(0);
+
+	const finishHandler = async (indicatedSegment) => {
+		let finalScore = 0;
+		if (indicatedSegment.text === 'JACKPOT') {
+			finalScore = jackpot;
+		} else {
+			finalScore = parseInt(indicatedSegment.text);
+		}
+		setBalance(balance + finalScore);
+		await doAPIRequest('/winners', 'post', {
+			user_id: 1, // hardcoded here but should be defined by auth
+			score: finalScore,
+			is_jackpot: indicatedSegment.text === 'JACKPOT' ? 1 : 0,
+			win_date: new Date().toISOString().split('T')[0]
+		});
+	}
+
+	// The way to connect wheel lib to react and keep it during the user session
+	const theWheel = React.useRef(
+		new Winwheel({
+			...wheelParams,
+			animation: {
+				type: 'spinToStop',
+				duration: 10, // Duration in seconds.
+				spins: 24,  // Default number of complete spins.
+				callbackFinished: finishHandler
+			},
+			
+		}));
 
 	React.useEffect(() => {
 		const getJackpot = async () => {
 			const { randomJackpot } = await doAPIRequest('/winners/jackpot');
 			setJackpot(randomJackpot);
-			// put this into local storage to be able to use this value in case user gets jackpot
-			localStorage.setItem('bears_jackpot', randomJackpot);
-			
 		}
 		getJackpot();
+		theWheel.current.draw();
 	}, []);
 
-	// -------------------------------------------------------
-	// Click handler for spin button.
-	// -------------------------------------------------------
+	// Click handler for spin button
 	function startSpin() {
-		// Ensure that spinning can't be clicked again while already running.
-		// if (!wheelSpinning) {
-
-			console.log('TODO: attach button to spinner component')
-			setBalance(balance + 123);
-			// // Begin the spin animation by calling startAnimation on the wheel object.
-			// theWheel.startAnimation();
-
-			// // Set to true so that power can't be changed and spin button re-enabled during
-			// // the current animation. The user will have to reset before spinning again.
-			// wheelSpinning = true;
-		// }
+		theWheel.current.startAnimation();
 	}
-
 
 	return (
 		<div className="row game">
 			<div className="col-6">
 				<div className={gameCSS.wrapper + ' float-end'}>
-					<Wheel />
+					<canvas id="canvas" width="434" height="434">
+						<p align="center">Sorry, your browser doesn't support canvas</p>
+					</canvas>
 					<img className={gameCSS.picker} src="./img/picker.png" alt="V" />
 				</div>
 			</div>
--- src/utils/doAPIRequest.js
+++ src/utils/doAPIRequest.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const doAPIRequest = async (path, method = 'get') => {
+const doAPIRequest = async (path, method = 'get', payload = null) => {
 	try {
 		const baseURL = process.env.REACT_APP_BACKEND_URL;
 		const response = await axios.request({
 			url: baseURL + path,
-			method: method.toLowerCase()
+			method: method.toLowerCase(),
+			data: payload,
 		})
 
 		const responseData = response.data;
